a0c4a06ceff342f6cc756186b40c276b11542e3a
Aleksander943 <aleksandededeus943@gmail.com>
2026-04-05 13:46:47 -0300

    feat: instalação do card do shadcn e componente de cadastro feito

---
+++ src/pages/Cadastro.tsx
@@ -0,0 +1,9 @@
+import { CardCadastro } from "@/components/Cadastro/cardPrincipal";
+
+export function Cadastro() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <CardCadastro />
+    </div>
+  );
+}
