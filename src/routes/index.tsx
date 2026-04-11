0a5c2f28c610396ccdbfdd6926013c687c88fec3
Aleksander943 <aleksandededeus943@gmail.com>
2026-04-10 22:05:27 -0300

    feat: criação da rota de login

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router';
 import { Cadastro } from '../pages/Cadastro';
+import { Login } from '../pages/Login';
 
 export function AppRoutes() {
 return (
 <Routes>
 <Route path="/" element={<div>Página Inicial</div>} />
 <Route path="/cadastro" element={<Cadastro />} />
+<Route path="/login" element={<Login />}/>
 </Routes>
 );
 }
