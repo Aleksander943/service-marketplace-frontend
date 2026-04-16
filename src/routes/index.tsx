d63d367338000e2e67324c04ce82fd1e51a61d44
Aleksander943 <aleksandededeus943@gmail.com>
2026-04-16 09:21:01 -0300

    feat: adicionando a rota para dashboard

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from 'react-router';
 import { Cadastro } from '../pages/Cadastro';
 import { Login } from '../pages/Login';
+import { Dashboard } from '@/pages/dashboard';
 
 export function AppRoutes() {
 return (
 <Routes>
-<Route path="/" element={<div>Página Inicial</div>} />
+<Route path="/" element={<Dashboard />} />
 <Route path="/cadastro" element={<Cadastro />} />
 <Route path="/login" element={<Login />}/>
 </Routes>
