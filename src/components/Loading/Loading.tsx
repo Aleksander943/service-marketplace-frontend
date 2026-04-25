68f12b4e4b0ebd3bb2bdc70a89da5a0326defcf4
Aleksander943 <aleksandededeus943@gmail.com>
2026-04-25 11:45:17 -0300

    feat: cria página de loading com spinner e badge

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -4,10 +4,6 @@ import { Spinner } from "@/components/ui/spinner"
 export function BadgeWithSpinner() {
   return (
     <div className="flex h-full items-center justify-center">
-      {/* <Badge variant="destructive">
-        <Spinner data-icon="inline-start" />
-        Deleting
-      </Badge> */}
       <Badge className="border-0 bg-transparent text-zinc-950 shadow-none hover:bg-transparent">
         carregando
         <Spinner data-icon="inline-end" />
