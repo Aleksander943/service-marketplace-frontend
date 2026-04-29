9fcf17b5b6f10274c2dea425c03155be9f2fef2b
Aleksander943 <aleksandededeus943@gmail.com>
2026-04-29 07:01:49 -0300

    feat: cria wrapper da página de revisão com navbar

---
+++ src/pages/RevisarService.tsx
@@ -0,0 +1,9 @@
+import { NavBar } from "@/components/NavBar/navBar";
+import RevisarServico from "@/components/Revisar/page";
+
+export const RevisarService = () => {
+    return(<div>
+        <NavBar/>
+        <RevisarServico/>
+    </div>)   
+};
