21ccd58d2962ae89305f662831349338637f5da5
Aleksander943 <aleksandededeus943@gmail.com>
2026-04-29 06:56:26 -0300

    refactor: atualiza rota de revisão para novo componente de página

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,26 +1,24 @@
-import { Routes, Route } from 'react-router';
-import { Cadastro } from '../pages/Cadastro';
-import { Login } from '../pages/Login';
-import { Dashboard } from '@/pages/dashboard';
-import { CreateService } from '@/pages/CreateService';
-import { PrivateRouter } from './PrivateRoute';
-import RevisarServico from '@/pages/RevisarServico';
-import PublicarServico from '@/pages/PublicarServico';
+import { Routes, Route } from "react-router";
+import { Cadastro } from "../pages/Cadastro";
+import { Login } from "../pages/Login";
+import { Dashboard } from "@/pages/dashboard";
+import { CreateService } from "@/pages/CreateService";
+import { PrivateRouter } from "./PrivateRoute";
 
-
+import PublicarServico from "@/pages/PublicarServico";
+import { RevisarService } from "@/pages/RevisarService";
 
 export function AppRoutes() {
-return (
-<Routes>
-<Route path="/" element={<Login />} />
-<Route path="/cadastro" element={<Cadastro />} />
-<Route element={<PrivateRouter />}>
-    <Route path="/dashboard" element={<Dashboard />} />
-    <Route path="/servicos/novo" element={<CreateService />} />
-    <Route path="/servicos/revisar" element={<RevisarServico />} />
-    <Route path="/servicos/publicar" element={<PublicarServico />} />
-    
-</Route>
-</Routes>
-);
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/cadastro" element={<Cadastro />} />
+      <Route element={<PrivateRouter />}>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/servicos/novo" element={<CreateService />} />
+        <Route path="/servicos/revisar" element={<RevisarService />} />
+        <Route path="/servicos/publicar" element={<PublicarServico />} />
+      </Route>
+    </Routes>
+  );
 }
