06ba9dbec37c38592ae89c6cbc7da9fbd2fd01a2
Aleksander943 <aleksandededeus943@gmail.com>
2026-05-03 11:03:49 -0300

    feat: mover ServiceFilter para dashboard page

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,10 +1,12 @@
 import { NavBar } from "@/components/NavBar/navBar";
 import { ServicesBoard } from "@/components/Dashboard/servicesBoard";
+import { ServiceFilter } from "@/components/NavBar/Filter/serviceFilter";
 
 export function Dashboard(){
     return(
         <div className="min-h-screen bg-[#ececea] pb-6">
             <NavBar/>
+            <ServiceFilter />
             <ServicesBoard />
         </div>
     )
