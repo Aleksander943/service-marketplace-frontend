8fc1eb49412c817d2f1e4c3c15828d665c7df0ed
Aleksander943 <aleksandededeus943@gmail.com>
2026-05-03 11:03:02 -0300

    feat: criar página Perfil com DashboardShowcase

---
+++ src/pages/Pefil.tsx
@@ -0,0 +1,12 @@
+import { DashboardShowcase } from "@/components/DashboardShowcase/dashboardShowcase";
+import { NavBar } from "@/components/NavBar/navBar";
+
+
+export const Perfil = () => {
+  return (
+    <div>
+       <NavBar />
+      <DashboardShowcase />
+    </div>
+  );
+};
