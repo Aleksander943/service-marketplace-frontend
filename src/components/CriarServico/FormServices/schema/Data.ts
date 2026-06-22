a7a7fca7c92137a4dad605ceac79fbfa5f17e1aa
Aleksander_De_Deus <aleksandededeus943@gmail.com>
2026-06-22 13:26:58 -0300

    refactor: ajuste no schema

--- src/components/CriarServico/FormServices/schema/Data.ts
+++ src/components/CriarServico/FormServices/schema/Data.ts
@@ -5,11 +5,20 @@ export const CadastroServicesSchema = z.object({
     .string()
     .min(5, "O título deve ter no mínimo 5 caracteres")
     .max(60, "O título deve ter no máximo 60 caracteres"),
+
   descricao: z
     .string()
     .min(5, "A descrição deve ter no mínimo 5 caracteres"),
+
   valor: z.coerce
-    .number("Informe um valor numérico")
+    .number()
     .min(1, "O valor mínimo é R$ 1,00"),
-  categoria: z.string().min(1, "Selecione uma categoria válida"),
+
+  categoria: z
+    .string()
+    .min(1, "Selecione uma categoria válida"),
+
+  image: z
+    .any()
+    .optional(),
 });
